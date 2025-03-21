18ae7c939874b5a38e5b2b2871e3417f195ddae5
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2025-03-21 12:00:33 +0530

    ecs

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -55,6 +55,6 @@ func Check(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{
-		"message": "ok aws-ecs",
+		"message": "ok aws-ecs-v2",
 	})
 }
